154e88c6a4521817ed21ae97b75da84f923e9d8e
prianshukr <priansh9877@gmail.com>
2025-02-08 00:49:24 +0530

    Create isPalindrome.java

---
+++ [07-02-2025]/DSA/isPalindrome.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            // Skip non-alphanumeric characters
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+
+            // Compare characters (ignoring case)
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
